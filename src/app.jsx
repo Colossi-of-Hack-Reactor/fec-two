ff4e29e56d3661ae2b504b467156fa7ed903ca46
arkteck <arkteck@gmail.com>
2022-05-22 11:17:07 -0400

    Added favicon and loading indicator. Finished routes for /products API endpoint. Added test that uses loading indicator.

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,54 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Overview from './overview/overview.jsx';
 import Related from './related/related.jsx';
 import Questions from './questions/questions.jsx';
 import Ratings from './ratings/ratings.jsx';
 
+const Loading = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    cursor: wait;
+`;
+
 function App() {
   const [product_id, setProduct_id] = useState(37311);
+  const [loading, setLoading] = useState(0);
 
   return (
     <>
+      {loading ? <Loading className="loading" data-testid="loading" /> : ''}
       <h1>Hello World</h1>
-      <Overview product_id={product_id} setProduct_id={setProduct_id}/>
-      <Related product_id={product_id} setProduct_id={setProduct_id} />
-      <Questions product_id={product_id} setProduct_id={setProduct_id} />
-      <Ratings product_id={product_id} setProduct_id={setProduct_id} />
+      <Overview
+        product_id={product_id}
+        setProduct_id={setProduct_id}
+        loading={loading}
+        setLoading={setLoading}
+      />
+      <Related
+        product_id={product_id}
+        setProduct_id={setProduct_id}
+        loading={loading}
+        setLoading={setLoading}
+      />
+      <Questions
+        product_id={product_id}
+        setProduct_id={setProduct_id}
+        loading={loading}
+        setLoading={setLoading}
+      />
+      <Ratings
+        product_id={product_id}
+        setProduct_id={setProduct_id}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </>
   );
 }
--- tests/overview.test.js
+++ tests/overview.test.js
@@ -13,23 +13,27 @@ axios.defaults.baseURL = 'http://localhost:3000';
 
 describe('Overview component', function() {
 
-  it('should increase the count after pressing the button', async () => {
-    render(<App />);
-    const user = userEvent.setup();
-    expect(screen.getByText('Overview:0')).toBeInTheDocument();
-    await user.click(screen.getByText('Click to increase OVERVIEW.'));
-    expect(screen.getByText('Overview:1')).toBeInTheDocument();
-  });
-
   it('should load product 37311 by default and change to different product when clicked', async () => {
     render(<App />);
     const user = userEvent.setup();
-    await waitForElementToBeRemoved(() => screen.queryByText('No products.'));
-    expect(screen.getByText('id:37311')).toBeInTheDocument();
-    await user.click(screen.getByText('37312'));
-    await waitFor(() => {
-      expect(screen.getByText('id:37312')).toBeInTheDocument();
-    });
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading'));
+    expect(screen.getByText('id: 37311')).toBeInTheDocument();
+    await user.click(screen.getByText('37312', {ignore: 'div'}));
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading'));
+    expect(screen.getByText('id: 37312')).toBeInTheDocument();
+  });
+
+  it('should load number of products based on input', async () => {
+    render(<App />);
+    const user = userEvent.setup();
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading'));
+    expect(screen.queryAllByTestId('product').length).toBe(5);
+    const countInput = screen.getByRole('spinbutton', {name: "Product Count:"});
+    expect(countInput).toHaveValue(5);
+    await user.clear(countInput);
+    await user.type(countInput, '10');
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading'));
+    expect(screen.queryAllByTestId('product').length).toBe(10);
   });
 
 });
