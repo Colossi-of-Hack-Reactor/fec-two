ddfb71bebc647042f03057985dd40230e5c2c0f9
Cory Nickerson <crnckrsn@gmail.com>
2022-05-24 17:09:13 -0400

    Modified helpful div and added functionality

--- src/questions/helpful.jsx
+++ src/questions/helpful.jsx
@@ -1,23 +1,42 @@
-
-
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
+import HelpfulVote from './helpfulvote.jsx';
 const HelpfulDiv = styled.div.attrs((props) => ({
   row: props.row,
 }))`
-  padding-top: 10px;
-  font-size: 12px;
-  font-weight: 200;
+display: grid;
+grid-template-columns: 5fr 2fr 4fr;
+  padding-top: 2px;
+  font-size: 10px;
+  font-weight: 100;
   grid-column-start: 2;
   grid-column-end: 3;
   grid-row-start: ${(props) => props.row};
   grid-row-end: ${(props) => props.row + 1};
 `;
+const Word = styled.div`
+padding-right: 0px;
+padding-top: 10px;
+font-size: 10px;
+font-weight: 100;
+grid-column-start: 1;
+`;
+const Votes = styled.div`
+padding-right: 0px;
+padding-top: 10px;
+font-size: 10px;
+font-weight: 100;
+grid-column-start: 3;
+`;
 
 const Helpful = function Helpful({ info, index }) {
+  console.log(info);
   return (
-    <HelpfulDiv row={index+1}>Helpful Here</HelpfulDiv>
+    <HelpfulDiv row={index + 1}>
+      <Word>Helpful?</Word>
+      <HelpfulVote />
+      <Votes>{'(' + info.question_helpfulness + ')'}</Votes>
+    </HelpfulDiv>
   );
 };
 export default Helpful;
