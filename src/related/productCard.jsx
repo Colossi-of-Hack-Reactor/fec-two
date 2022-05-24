349c3dbf8663af76145409f5d93f5c27078ecd66
Irvin Solano <irvin.solano.ny@gmail.com>
2022-05-24 12:37:13 -0400

    Edited - refresh on new id provided, thumbnails of default products showing unless unavailable, alt text provided for all images

--- src/related/productCard.jsx
+++ src/related/productCard.jsx
@@ -4,22 +4,25 @@
 /* eslint-disable camelcase */
 import React, { useState, useEffect } from 'react';
 
-const Productcards = ({ product, cards, totalStyles, defaultIndex }) => {
-  console.log(product)
+const Productcards = ({ product = {}, cards = [], defaultIndex }) => {
   const [index, setIndex] = useState(defaultIndex);
-  useEffect(() => {
-    setIndex(defaultIndex);
-  }, ([]));
+  useEffect((newIndex) => {
+    setIndex(newIndex || defaultIndex);
+  }, ([index]));
+
+  const totalStyles = cards.length;
+  const allStyles = cards.reduce((memo, card, i) => (i !== cards.length - 1 ? `${memo} ${card.style_name},` : `${memo} ${card.style_name}.`), '');
+
   return (
     <div>
       <div>ActionButton and image are in same div</div>
-      <img alt="" src="/assets/android-chrome-192x192.png" />
+      <img alt={index !== undefined ? `${cards[index].style_name}, category is ${cards[index].category}` : `Sorry, no physical description is available for this item. Product category is ${product.category} and comes in the following styles: ${allStyles}`} src={(index !== undefined && cards[index].photos[0].thumbnail_url) ? cards[index].photos[0].thumbnail_url : '/assets/android-chrome-192x192.png'} />
       <br />
       {product.name}
       <br />
       {product.category}
       <br />
-      {index ? cards[index].original_price : product.default_price}
+      {index !== undefined ? cards[index].original_price : product.default_price}
       <br />
       Ratings go here
     </div>
