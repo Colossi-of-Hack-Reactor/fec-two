bd7b24fb8d9320ef9ec3b68ab080ed957ab8a386
Irvin Solano <irvin.solano.ny@gmail.com>
2022-05-24 13:24:52 -0400

    fixed eslint errors

--- src/related/related.jsx
+++ src/related/related.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 import ProductCard from './productCard.jsx';
 
 function Related({ product_id }) {
@@ -15,38 +16,42 @@ function Related({ product_id }) {
             Promise.allSettled(relatedIds.map((id) => axios.get(`/products/${id}/styles`)))
               .then((promisesArr) => promisesArr.map((res) => (res.status === 'fulfilled' ? res.value.data : {})))
               .then((data) => productsArr.map((product, i) => Object.assign(data[i], product)))
-              .then(finalProductArr => setProducts(finalProductArr));
+              .then((finalProductArr) => setProducts(finalProductArr));
           });
       })
       .catch((err) => console.log('FAILURE', err));
   }, []);
-  return <div>
-    {products.map((product) => {
-      const altCards = [];
-      product.results.forEach((altStyle) => {
-        const {
-          name, original_price, photos, sale_price = 0,
-        } = altStyle;
 
-        altCards.push({
-          default: altStyle['default?'],
-          category: product.category,
-          style_name: name,
-          original_price,
-          sale_price,
-          photos,
+  return (
+    <div>
+      {products.map((product) => {
+        const altCards = [];
+        product.results.forEach((altStyle) => {
+          const {
+            name, original_price, photos, sale_price = 0,
+          } = altStyle;
+
+          altCards.push({
+            default: altStyle['default?'],
+            category: product.category,
+            style_name: name,
+            original_price,
+            sale_price,
+            photos,
+          });
         });
-      });
-      let defaultIndex;
-      altCards.forEach((card, i) => {
-        if(card.default) {
-          defaultIndex = i;
-        }
-      })
-      return <ProductCard product={product} cards={altCards} totalStyles={altCards.length} defaultIndex={defaultIndex} key={product.id} />;
-    })
-    }
-  </div>;
+        let defaultIndex;
+        altCards.forEach((card, i) => {
+          if (card.default) {
+            defaultIndex = i;
+          }
+        });
+        return (
+          <ProductCard product={product} cards={altCards} defaultIndex={defaultIndex} key={product.id} />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Related;
