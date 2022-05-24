086fbace4d9dd15becf96748ca621730bd849be7
Yao Yu <amyyuyao.thu@gmail.com>
2022-05-24 13:26:20 -0400

    fix the meta.characteristics mapping

--- src/ratings/ratingList.jsx
+++ src/ratings/ratingList.jsx
@@ -1,39 +1,40 @@
 import React, { useState, useEffect } from 'react';
+// import Flex from './flex.styled.jsx';
 
 export default function RatingList({ meta }) {
   return (
-    <div>
-      {
-        Object.keys(meta).length !== 0
-          ? (
-            <div>
-              <hr />
+    // <Flex>
+      <div>
+        {
+          Object.keys(meta).length !== 0
+            ? (
               <div>
-                <span>{meta.ratings[1]}</span>
-                <span>{meta.ratings[2]}</span>
-                <span>{meta.ratings[3]}</span>
-                <span>{meta.ratings[4]}</span>
-                <span>{meta.ratings[5]}</span>
+                <hr />
+                <div>
+                  <span>{meta.ratings[1]}</span>
+                  <span>{meta.ratings[2]}</span>
+                  <span>{meta.ratings[3]}</span>
+                  <span>{meta.ratings[4]}</span>
+                  <span>{meta.ratings[5]}</span>
+                </div>
+                <hr />
+                <div>
+                  {
+                    meta.characteristics
+                      ? Object.keys(meta.characteristics).map((char, i) => (
+                        <div key={i}>
+                          <span>{char}</span>
+                          {' '}
+                          <span>{meta.characteristics[char].value}</span>
+                        </div>
+                      )) : (null)
+                  }
+                </div>
+                <hr />
               </div>
-              <hr />
-              <div>
-                <span>
-                  {meta.characteristics.Comfort.value}
-                </span>
-                <span>
-                  {meta.characteristics.Fit.value}
-                </span>
-                <span>
-                  {meta.characteristics.Length.value}
-                </span>
-                <span>
-                  {meta.characteristics.Quality.value}
-                </span>
-              </div>
-              <hr />
-            </div>
-          ) : (null)
-      }
-    </div>
+            ) : (null)
+        }
+      </div>
+    // </Flex>
   );
 }
