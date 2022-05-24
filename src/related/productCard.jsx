b86f84b30a4951ff2f3421ef7fbb5250e5283f83
Irvin Solano <irvin.solano.ny@gmail.com>
2022-05-24 04:10:58 -0400

    ProductCards uses api data to create one card for each default related item

--- src/related/productCard.jsx
+++ src/related/productCard.jsx
@@ -2,13 +2,28 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable camelcase */
+import React, { useState, useEffect } from 'react';
 
-const React = require('react');
-
-const { useState, useEffect } = require('react');
-
-const ProductCard = (props) => {
-console.log('yay')
+const Productcards = ({ product, cards, totalStyles, defaultIndex }) => {
+  console.log(product)
+  const [index, setIndex] = useState(defaultIndex);
+  useEffect(() => {
+    setIndex(defaultIndex);
+  }, ([]));
+  return (
+    <div>
+      <div>ActionButton and image are in same div</div>
+      <img alt="" src="/assets/android-chrome-192x192.png" />
+      <br />
+      {product.name}
+      <br />
+      {product.category}
+      <br />
+      {index ? cards[index].original_price : product.default_price}
+      <br />
+      Ratings go here
+    </div>
+  );
 };
 
-module.exports = { ProductCard };
+export default Productcards;
