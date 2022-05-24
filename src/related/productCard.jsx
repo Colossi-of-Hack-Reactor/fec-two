a78a5302785c78f50f70051b194352372a6445c7
Irvin Solano <irvin.solano.ny@gmail.com>
2022-05-23 22:46:10 -0400

    Related gathers details for ALL related product and saves to state

---
+++ src/related/productCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable camelcase */
+
+const React = require('react');
+
+const { useState, useEffect } = require('react');
+
+const ProductCard = (props) => {
+console.log('yay')
+};
+
+module.exports = { ProductCard };
