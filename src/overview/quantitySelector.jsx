0ea5c438d1ed9265c233a6d245555fcc145d9bc5
arkteck <arkteck@gmail.com>
2022-05-23 22:53:39 -0400

    Added basic functionality of style selector and image gallery.

--- src/overview/quantitySelector.jsx
+++ src/overview/quantitySelector.jsx
@@ -4,23 +4,25 @@ function QuantitySelector(props) {
   const { setQuantity, size, style, styles } = props;
 
   return (
-    <label>
-      Select Quantity
-      {' '}
-      <select
-        name="quantity"
-        onChange={(e) => {
-          setQuantity(e.target.value);
-        }}
-        disabled={size === null}
-      >
-        {size === null || styles[style].skus[size] === undefined ? <option>-</option>
-          : Array(Math.min(15, styles[style].skus[size].quantity))
-            .fill(0).map((v, i) => i + 1).map((q) => (
-              <option key={q} value={q}>{q}</option>
-            ))}
-      </select>
-    </label>
+    <div>
+      <label>
+        Select Quantity
+        {' '}
+        <select
+          name="quantity"
+          onChange={(e) => {
+            setQuantity(e.target.value);
+          }}
+          disabled={size === 'Select'}
+        >
+          {size === 'Select' || styles[style].skus[size] === undefined ? <option>-</option>
+            : Array(Math.min(15, styles[style].skus[size].quantity))
+              .fill(0).map((v, i) => i + 1).map((q) => (
+                <option key={q} value={q}>{q}</option>
+              ))}
+        </select>
+      </label>
+    </div>
   );
 }
 
--- src/overview/styleSelector.jsx
+++ src/overview/styleSelector.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TD = styled.td`
+  position: relative
+`;
+
+const StyleImage = styled.img`
+  cursor: pointer;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+`;
+
+const Check = styled.span`
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  text-shadow: 2px 2px;
+`;
 
 function StyleSelector(props) {
-  const { setStyle, styles } = props;
+  const { style, setStyle, styles } = props;
+  const stylesArray = [];
+
+  for (let i = 0; i < styles.length; i += 4) {
+    stylesArray.push(styles.slice(i, i + 4));
+  }
 
   return (
-    <label>
-      Style
-      {' '}
-      <select
-        name="style"
-        onChange={(e) => {
-          setStyle(e.target.value);
-        }}
-      >
-        {styles.map((s, i) => (
-          <option value={i} key={s.name}>
-            {s.name}
-          </option>
-        ))}
-      </select>
-    </label>
+    <>
+      <span>
+        Style:
+        {' '}
+        {styles[style].name}
+      </span>
+      <table>
+        <tbody>
+          {stylesArray.map((row, i) => (
+            <tr key={i}>
+              {row.map((s, j) => (
+                <TD key={s.style_id}>
+                  <StyleImage
+                    className={4 * i + j === style ? 'selected' : null}
+                    key={s.style_id}
+                    src={s.photos[0].thumbnail_url}
+                    onClick={() => setStyle(4 * i + j)}
+                  />
+                  {4 * i + j === style ? <Check>☑️</Check> : ''}
+                </TD>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
