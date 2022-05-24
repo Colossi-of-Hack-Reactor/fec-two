4117f59a4742fee17d37df4beb9627bec28d4f82
Irvin Solano <irvin.solano.ny@gmail.com>
2022-05-24 00:18:06 -0400

    Related updates state to compiled products info

--- src/related/related.jsx
+++ src/related/related.jsx
@@ -14,12 +14,12 @@ function Related({ product_id }) {
             Promise.allSettled(relatedIds.map((id) => axios.get(`/products/${id}/styles`)))
               .then((promisesArr) => promisesArr.map((res) => (res.status === 'fulfilled' ? res.value.data : {})))
               .then((data) => productsArr.map((product, i) => Object.assign(data[i], product)))
-              .then(finalProductArr => console.log(finalProductArr));
+              .then(finalProductArr => setProducts(finalProductArr));
           });
       })
       .catch((err) => console.log('FAILURE', err));
   }, []);
-
+  return console.log(products)
   // return <div>{products.map((product) => <ProductCard product={product} key={product.id} />)}</div>;
 }
 
