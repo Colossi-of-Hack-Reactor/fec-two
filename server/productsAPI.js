cdb25a89fe9e115332ae506e548c6a4973a400c9
arkteck <arkteck@gmail.com>
2022-06-02 13:46:19 -0400

    Bot for scraping API for product and sale data.

--- server/productsAPI.js
+++ server/productsAPI.js
@@ -1,13 +1,73 @@
 const axios = require('axios');
-
+const mongoose = require("mongoose");
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
 const headers = {
   Authorization: process.env.GITHUBKEY,
 };
 
+mongoose.connect(`mongodb+srv://${process.env.mongoUN}:${process.env.mongoPW}@cluster0.y9iir.mongodb.net/fec?retryWrites=true&w=majority`);
+const productSchema = new mongoose.Schema({
+  id: Number,
+  campus: String,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: String,
+  created_at: String,
+  updated_at: String,
+});
+const Product = mongoose.model('Products', productSchema);
+
+const addProduct = function(product) {
+  return Product.create({
+    id: product.id,
+    campus: product.campus,
+    name: product.name,
+    slogan: product.slogan,
+    description: product.description,
+    category: product.category,
+    default_price: product.default_price,
+    created_at: product.created_at,
+    updated_at: product.updated_at,
+  });
+};
+
+const saleSchema = new mongoose.Schema({
+  product_id: Number,
+  style_id: Number,
+  name: String,
+  original_price: String,
+  sale_price: String,
+});
+
+const Sales = mongoose.model('Sales', saleSchema);
+
+const addSales = function(sale) {
+  return Sales.create({
+    product_id: sale.product_id,
+    style_id: sale.style_id,
+    name: sale.name,
+    original_price: sale.original_price,
+    sale_price: sale.sale_price,
+  });
+};
+
 exports.getProducts = (req, res) => {
   axios.get(`${baseURL}/products`, { headers, params: req.query })
     .then((response) => {
+      const products = response.data;
+      for (let i = 0; i < products.length; i++) {
+        addProduct(products[i])
+          .then(() => {
+            console.log(products[i].id);
+          })
+          .catch(err => {
+            console.log('error on ', products[i].id);
+            console.log(err);
+          });
+      }
+
       res.status(200).send(response.data);
     })
     .catch((err) => {
@@ -28,7 +88,27 @@ exports.getProduct = (req, res) => {
 exports.getStyles = (req, res) => {
   axios.get(`${baseURL}/products/${req.params.product_id}/styles`, { headers })
     .then((response) => {
-      res.status(200).send(response.data);
+      const pid = response.data.product_id;
+      const styles = response.data.results;
+      for (let i = 0; i < styles.length; i++) {
+        if (styles[i].sale_price) {
+          addSales({
+            product_id: pid,
+            style_id: styles[i].style_id,
+            name: styles[i].name,
+            original_price: styles[i].original_price,
+            sale_price: styles[i].sale_price,
+          })
+            .then(() => {
+              console.log(pid);
+            })
+            .catch(err => {
+              console.log('error on ', pid);
+              console.log(err);
+            });
+        }
+      }
+      res.status(200).send(response.data.results);
     })
     .catch((err) => {
       res.status(404).send(err);
--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,7 @@
 /* eslint-disable camelcase */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-import Overview from './overview/overview.jsx';
-import Related from './related/related.jsx';
-import Questions from './questions/questions.jsx';
-import Ratings from './ratings/ratings.jsx';
-import ShoppingBag from './shoppingBag.jsx';
-import { ShoppingBagSVG } from './overview/overviewAssets.js';
+import Bot from './bot/bot.jsx';
 
 const Loading = styled.div`
   position: fixed;
@@ -81,109 +75,8 @@ const Badge = styled.div`
 `;
 
 function App() {
-  const [product_id, setProduct_id] = useState(37311);
-  const [loading, setLoading] = useState(0);
-  const [outfitsIdList, setOutfitsIdList] = useState([]);
-  const [outfits, setOutfits] = useState({});
-  const [reviews, setReviews] = useState([]);
-  const [meta, setMeta] = useState({});
-  const [product, setProduct] = useState({});
-  const ratingsRef = React.useRef();
-  const [cart, setCart] = useState([]);
-  const [showCart, setShowCart] = useState(false);
-
-  useEffect(() => {
-    function handleInteraction(e) {
-      let node = e.target;
-      let widget = 'Other';
-      while (node.parentNode !== null) {
-        if (/Overview/.test(node.className)) {
-          widget = 'Overview';
-          break;
-        } else if (/Related/.test(node.className)) {
-          widget = 'Related';
-          break;
-        } else if (/Questions/.test(node.className)) {
-          widget = 'Questions';
-          break;
-        } else if (/Ratings/.test(node.className)) {
-          widget = 'Ratings';
-          break;
-        }
-        node = node.parentNode;
-      }
-      axios.post('/interactions', {
-        element: e.target.outerHTML,
-        widget,
-        time: Date(),
-      })
-        .catch((err) => {
-          console.log('axios post interactions error', err);
-        });
-    }
-    window.addEventListener('click', handleInteraction);
-    return () => window.removeEventListener('click', handleInteraction);
-  }, []);
-
   return (
-    <>
-      {loading ? <Loading className="loading" data-testid="loading" /> : ''}
-      <Header>
-        <HeaderDiv>
-          <CompanyName>
-            Colossus of Clothes
-          </CompanyName>
-          <Promos>
-            <div>Free express shipping on $200+ orders for rewards members!</div>
-            <div>Sign up now!</div>
-          </Promos>
-          <ShoppingBagDiv onClick={() => setShowCart((a) => !a)}>
-            <ShoppingBagSVG />
-            {cart.length ? (
-              <Badge>
-                <h6>{cart.length}</h6>
-              </Badge>
-            ) : ''}
-          </ShoppingBagDiv>
-        </HeaderDiv>
-      </Header>
-      <Overview
-        product_id={product_id}
-        setProduct_id={setProduct_id}
-        setLoading={setLoading}
-        outfitsIdList={outfitsIdList}
-        setOutfitsIdList={setOutfitsIdList}
-        reviews={reviews}
-        meta={meta}
-        ratingsRef={ratingsRef}
-        product={product}
-        setProduct={setProduct}
-        cart={cart}
-        setCart={setCart}
-      />
-      <Related
-        product_id={product_id}
-        setProduct_id={setProduct_id}
-        setLoading={setLoading}
-      />
-      <Questions
-        product_id={product_id}
-        setProduct_id={setProduct_id}
-        setLoading={setLoading}
-      />
-      <Ratings
-        product_id={product_id}
-        setProduct_id={setProduct_id}
-        setLoading={setLoading}
-        reviews={reviews}
-        setReviews={setReviews}
-        meta={meta}
-        setMeta={setMeta}
-        ratingsRef={ratingsRef}
-        product={product}
-      />
-      <ShoppingBag show={showCart} cart={cart} setCart={setCart} />
-    </>
+    <Bot />
   );
 }
 
